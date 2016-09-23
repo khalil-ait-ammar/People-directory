9557c24d3264a9b2f6b7081605c7ead7459509a3
khalilaitammar <ait.ammar.khalil@gmail.com>
2016-09-23 18:15:15 +0200

    initialisation du projet

---
+++ app/app.js
@@ -0,0 +1,13 @@
+
+'use strict';
+
+/**
+ * Déclaration de l'application pDirectory
+ */
+
+
+var pDirectory = angular.module('pDirectory', []);
+
+pDirectory.controller('helloCtrl', ['$scope', function ($scope) {
+
+}]);
