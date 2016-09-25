fc47a4921d008b8315a13ac6f9dc15a410709e9a
khalilaitammar <ait.ammar.khalil@gmail.com>
2016-09-25 21:05:03 +0200

    ajout du system de route & rectification graphique

--- app/app.js
+++ app/app.js
@@ -6,8 +6,46 @@
  */
 
 
-var pDirectory = angular.module('pDirectory', []);
+var Directory = angular.module('Directory', [
+    // Dépendances du "module"
+    'ngRoute',
+    'MainCtrl'
+]);
 
-pDirectory.controller('helloCtrl', ['$scope', function ($scope) {
 
-}]);
+/**
+ * Configuration du module principal : routeApp
+ */
+Directory.config(['$routeProvider',
+    function($routeProvider) {
+
+        // Système de routage
+        $routeProvider
+            .when('/home/:msg?', {
+                templateUrl: 'pages/home.html',
+                controller: 'MainCtrl'
+            })
+            .when('/contact/:msg?', {
+                templateUrl: 'pages/contact.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+]);
+
+
+var MainCtrl = angular.module('MainCtrl', []);
+
+
+
+Directory.controller('MainCtrl', ['$scope','$routeParams',
+    function($scope, $routeParams){
+        $scope.message = "Bienvenue sur la page d'accueil";
+        $scope.isActive = false;
+        $scope.activeButton = function() {
+            $scope.isActive = !$scope.isActive;
+        }
+    }
+]);
---
+++ controllers/loginController.js
@@ -0,0 +1,80 @@
+'use strict';
+app.controller('loginController', function ($scope, $http, $window, $cookieStore, toastr) {
+
+
+    $scope.logout = function () {
+        $http.get('/user/logout')
+            .success(function (data, status) {
+                if (status === 200 && data.status) {
+                    $scope.formup = true;
+                    $cookieStore.remove('myCookies');
+                    console.log('lougout');
+                } else {
+                    console.log('erreur lougout');
+                }
+            })
+            // handle error
+            .error(function (data) {
+                console.log('erreur lougout');
+            });
+    }
+
+
+    $scope.sub = function () {
+
+        console.log("doné user", $scope.login, $scope.mp);
+
+        var data = $.param({
+            json: JSON.stringify({
+                username: $scope.login,
+                password: $scope.mp
+            })
+        });
+
+        $http.post('/user/login',
+            {username: $scope.login, password: $scope.mp})
+
+            .success(function (data, status, header) {
+                if (status === 200) {
+                    $scope.formup = false;
+                    $cookieStore.put('myCookies', data);
+                    console.log('cookies',data);
+                    toastr.success('Authentification réussie bienvenue ' + data.username, 'success!');
+
+                } else {
+                    console.log('loula erreur login');
+                    toastr.error('login ou mot de passe incorrects', 'Error');
+                }
+            })
+
+            .error(function (data) {
+                console.log(' 2 erreur login');
+                toastr.error('login ou mot de passe incorrects', 'Error');
+            });
+
+
+    }
+    $scope.register = function () {
+        $http.post('/user/register', {username: $scope.username, password: $scope.passuser, usermail: $scope.usermail})
+            .success(function (data, status) {
+                console.log('register succés', data, status);
+                var iduserdata = data;
+
+                $http.post('/api/panier', {iduser: iduserdata})
+                    .success(function (data, status) {
+                        console.log('panier', data);
+                        toastr.success('utilisateur crée  ' + $scope.username, 'success!');
+                    })
+                    .error(function (data, status) {
+                        toastr.error('erreur lors de lenregistrement', 'Error');
+                    });
+
+            })
+            .error(function (data, status) {
+                toastr.error('erreur lors de lenregistrement', 'Error');
+
+            })
+
+    }
+
+});
